71635d2573791392c05e4a09a2649dc2531fd558
Ellen <ellen.deveth@hotmail.com>
2022-04-28 13:54:24 +0200

    feat: make sure that an email is correct

--- models/users.js
+++ models/users.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: String,
-    email: String,
+    email: {
+        type: String, 
+        unique: true, 
+        match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    },
     password: String,
 });
 
