ec58f3afcdfee0278a818e11d47c1404d5652b8f
Ellen <ellen.deveth@hotmail.com>
2022-04-28 13:32:09 +0200

    feat: add user schema for database

--- models/users.js
+++ models/users.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String
+    username: String,
+    email: String,
+    password: String,
 });
 
 const User = mongoose.model('User', userSchema);
