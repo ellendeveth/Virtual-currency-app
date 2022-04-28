2a53c66654d6d88b6649af521616aaf384ef2dc5
Ellen <ellen.deveth@hotmail.com>
2022-04-28 13:48:46 +0200

    feat: add function to see if user already exists

--- controllers/users.js
+++ controllers/users.js
@@ -3,42 +3,48 @@ const User = require('../models/users');
 const bcrypt = require('bcrypt');
 
 const signup = (req, res, next) => {
-    // const { username, password } = req.body;
-    // const user = new User({ username, password });
-    // user.save()
-    //     .then(() => res.json({ message: 'User created!' }))
-    //     .catch(err => next(err));
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            console.log(err);
+    User.find({email: req.body.email}).exec().then(user => {
+        if (user.length >= 1) {
+            //er bestaat al een user met deze email
             return res.json({
-                "status": "error",
-            });
+                status: 'error',
+                message: 'User already exists'
+            })
         } else {
-            const user = new User({
-                username: req.body.username, 
-                email: req.body.email, 
-                password: hash
-            });
-            user.save()
-            .then(result => {
-                console.log(result);
-                res.json({
-                    "status": 'success',
-                    "data": {
-                        "username": result.username,
-                        "password": result.password,
-                    }
-                });
-            }).catch(err => {
-                console.log(err);
-                res.json({
-                    "status": 'error'
-                })
-            });
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    console.log(err);
+                    return res.json({
+                        "status": "error",
+                    });
+                } else {
+                    const user = new User({
+                        username: req.body.username, 
+                        email: req.body.email, 
+                        password: hash
+                    });
+                    user.save()
+                    .then(result => {
+                        console.log(result);
+                        res.json({
+                            "status": 'success',
+                            "data": {
+                                "username": result.username,
+                                "password": result.password,
+                            }
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                        res.json({
+                            "status": 'error'
+                        })
+                    });
+                }
+            })
+            
         }
-    })
-    
+    }); 
+   
 };
 
 const login = (req, res, next) => {
