a0b3946a9e0dca2ea9f2633802a0520f44ac3762
Ellen <ellen.deveth@hotmail.com>
2022-04-30 09:48:36 +0200

    feat: add routes to login and signup page

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const authorization = require('../controllers/users');
+const checkAuth = require('../middleware/check-auth');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/test', checkAuth, function(req, res, next) {
+  res.send('respond with a resource');
+  });
 
 router.post('/signup', authorization.signup);
 router.post('/login', authorization.login);
