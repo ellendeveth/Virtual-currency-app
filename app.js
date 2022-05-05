5d7c009a0b1bff972d7c907c9323bb50c018ca24
Ellen <ellen.deveth@hotmail.com>
2022-05-05 11:52:11 +0200

    feat: setup transactions

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var transactionsRouter = require('./routes/transactions');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/currencyapp');
@@ -27,6 +28,7 @@ app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/transactions', transactionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
