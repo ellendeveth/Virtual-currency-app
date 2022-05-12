e9d96aad1dbe7568ef28104eea2d599980ef6ef0
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-12 12:06:21 +0200

    feat: add cors to project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
