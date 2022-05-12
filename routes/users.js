df5d8b4082feb2004e61a963fd710848d66e93e7
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-12 12:07:42 +0200

    feat: add route to get amount of user

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const authorization = require('../controllers/users');
+const authorization = require('../controllers/auth');
+const users = require('../controllers/users');
 const checkAuth = require('../middleware/check-auth');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// get all users
+router.get('/', checkAuth, users.getUser);
+// get user amount
+router.get('/amount', checkAuth, users.getAmount);
 
 router.post('/test', checkAuth, function(req, res, next) {
   res.send('respond with a resource');
