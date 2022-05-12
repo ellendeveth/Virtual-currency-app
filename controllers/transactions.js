05d0177997ff61a562309872f114b8b8fcd9232a
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-12 12:09:15 +0200

    feat: add function to get transaction by id

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -5,16 +5,32 @@ const add = (req, res, next) => {
     const transaction = new Transaction({
         amount: req.body.amount,
         sender: req.body.sender,
-        receiver: req.body.receiver
+        receiver: req.body.receiver,
+        reason: req.body.reason
     });
-    transaction.save();
+    transaction.save((err, result)=>{
+        if (err) {
+            res.json({
+                "status": 'error',
+                "message": 'Could not make the transaction'
+            })
+        } else {
+            res.json({
+                "status": 'success',
+                "message": 'Transaction made',
+                "data": {
+                    "transaction": result,
+                }
+            })
+        }
+    });
     res.send('Transaction added');
 
 }
 
-const get = (req, res, next) => {
+const getAll = (req, res, next) => {
     // get all transactions
-    Transaction.find({"sender": "Alejandro"}, (err, docs)=>{
+    Transaction.find((err, docs)=>{
         if(!err){
             res.json({
                 "status": "success",
@@ -23,10 +39,33 @@ const get = (req, res, next) => {
                     
                 }
             })
+        } else {
+            res.json({
+                "status": "error"
+            })
         }
     })
 
 }
 
+const getById = (req, res, next) => {
+    // get transaction by id
+    Transaction.findById(req.params.id, (err, doc)=>{
+        if(!err){
+            res.json({
+                "status": "success",
+                "data": {
+                    "transaction": doc,
+                }
+            })
+        } else {
+            res.json({
+                "status": "error"
+            })
+        }
+    })
+}
+
 module.exports.add = add;
-module.exports.get = get;
+module.exports.getAll = getAll;
+module.exports.getById = getById;
