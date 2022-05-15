7232557db1b169ecd07edeb26dbba60b1b75033c
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-15 15:48:55 +0200

    refactor: change username to firstname and lastname in Mongoose model

--- models/User.js
+++ models/User.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String,
+    firstname: String,
+    lastname: String,
     email: {
-        type: String, 
-        unique: true, 
+        type: String,
+        unique: true,
         match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
     },
     password: String,
