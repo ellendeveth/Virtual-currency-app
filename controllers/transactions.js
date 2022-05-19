97925576c38ef735a74db7035eefcac697a48447
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-19 22:47:38 +0200

    feat: add a transaction from one to another

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,35 +1,62 @@
 const Transaction = require('../models/Transaction');
 const jwt = require("jsonwebtoken");
+const User = require('../models/User');
 
-const add = (req, res, next) => {
+const add = async (req, res, next) => {
     let token = req.headers.authorization;
     let decode = jwt.decode(token).userId;
-   
+
+    let amount = req.body.amount
+    let receiver = req.body.receiver
+    let reason = req.body.reason
+
     // make transaction possible
     const transaction = new Transaction({
-        amount: req.body.amount,
+        amount: amount,
         sender: decode,
-        receiver: req.body.receiver,
-        reason: req.body.reason
+        receiver: receiver ,
+        reason: reason
     });
-    transaction.save((err, result)=>{
-        if (err) {
+    
+    User.findById(decode, (err, doc)=>{
+        if(doc.balance < amount ){
             res.json({
-                "status": 'error',
-                "message": 'Could not make the transaction'
+                "status": "error",
+                "message": "You don't have enough money"
             })
         } else {
-            res.json({
-                "status": 'success',
-                "message": 'Transaction made',
-                "data": {
-                    "transaction": result,
-                }
-            })
-        }
-    });
-    res.send('Transaction added');
+            //update sender balance
+            let newAmount = doc.balance - amount;
+            User.findByIdAndUpdate(decode, {balance: newAmount}, (err, doc)=>{
 
+                //find receiver
+                User.find({"firstname":receiver}, (err, doc) =>{
+                    let oldAmount = doc[0].balance;
+                    let newAmountreceiver = oldAmount + parseInt(amount);
+
+                    //update receiver balance
+                    User.findByIdAndUpdate(doc[0]._id, {balance: newAmountreceiver}, (err, doc)=>{ 
+                        transaction.save((err, result)=>{
+                            if (err) {
+                                res.json({
+                                    "status": 'error',
+                                    "message": 'Could not make the transaction'
+                                })
+                            } else {
+                                res.json({
+                                    "status": 'success',
+                                    "message": 'Transaction made',
+                                    "data": {
+                                        "transaction": result,
+                                    }
+                                })
+                            }
+                        })
+                    })
+                })         
+            }) 
+        }
+    })
 }
 
 const getAll = (req, res, next) => {
