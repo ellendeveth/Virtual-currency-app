582371420fffe8f8ee05e0e10ffa897d87ea78fe
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-05-19 09:36:10 +0200

    feat: connect app to online Atlas database

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 var transactionsRouter = require('./routes/transactions');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/currencyapp');
+mongoose.connect('mongodb+srv://alejandrodewolf:B5LuQogI4RfW2lxM@cluster0.iaqvg.mongodb.net/?retryWrites=true&w=majority');
 
 var app = express();
 
@@ -33,12 +33,12 @@ app.use('/users', usersRouter);
 app.use('/transactions', transactionsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
